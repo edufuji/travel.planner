648ba035017f9c6a9f398352b672609e3866ed1f
Eduardo Fujiwara <eduardoshoitifujiwara@gmail.com>
2026-03-19 19:55:54 -0300

    feat: add AnimatePresence to App for page transitions

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { AnimatePresence } from 'framer-motion'
+import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom'
 import { AuthProvider } from '@/contexts/AuthContext'
 import { useDestinationsSync } from '@/hooks/useDestinationsSync'
 import ProtectedRoute from '@/components/ProtectedRoute'
@@ -10,15 +11,18 @@ import ProfilePage from '@/pages/ProfilePage'
 
 function AppRoutes() {
   useDestinationsSync()
+  const location = useLocation()
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/trips" replace />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignupPage />} />
-      <Route path="/trips" element={<ProtectedRoute><TripsPage /></ProtectedRoute>} />
-      <Route path="/trips/:id" element={<ProtectedRoute><TripDetailPage /></ProtectedRoute>} />
-      <Route path="/profile" element={<ProtectedRoute><ProfilePage /></ProtectedRoute>} />
-    </Routes>
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Navigate to="/trips" replace />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/trips" element={<ProtectedRoute><TripsPage /></ProtectedRoute>} />
+        <Route path="/trips/:id" element={<ProtectedRoute><TripDetailPage /></ProtectedRoute>} />
+        <Route path="/profile" element={<ProtectedRoute><ProfilePage /></ProtectedRoute>} />
+      </Routes>
+    </AnimatePresence>
   )
 }
 
